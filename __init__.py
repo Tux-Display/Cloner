f53876f46f3eda5ecb4370e02c312c239a4b65c3
Crunch <91413622+CrunchV1@users.noreply.github.com>
2022-04-06 15:03:09 +0200

    ffff
    f

--- __init__.py
+++ __init__.py
@@ -1,15 +1,16 @@
 from mycroft import MycroftSkill, intent_file_handler
+import os
 
-
-class ClockForSchoolDieWantIi(MycroftSkill):
+class cloner(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    @intent_file_handler('ii.want.die.school.for.clock.intent')
+    @intent_file_handler('cloner.intent')
     def handle_ii_want_die_school_for_clock(self, message):
-        self.speak_dialog('ii.want.die.school.for.clock')
+        self.speak_dialog('cloner')
+        os.system("for repository in $(cat /home/pi/repositories.txt); do rm -rf /home/pi/mycroft-core/skills/$repository; git clone $repository /home/pi/mycroft-core/skills/$repository; done")
 
 
 def create_skill():
-    return ClockForSchoolDieWantIi()
+    return cloner()
 
